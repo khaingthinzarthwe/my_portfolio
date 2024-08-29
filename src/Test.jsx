e7147d2dff274c647ba435fa2d8449d689f5c0f1
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-29 16:19:00 +0700

    Framer Motion Test with Variants and State

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,15 +1,23 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Test = () => {
+    const [open, setOpen] = useState(false);
+
+    const variants = {
+        visible: {opacity: 1, x: 500, transition: {type: "spring", stiffness: 100, damping: 50 }},
+        hidden: {opacity: 0}
+    }
   return (
     <div className="course">
       <motion.div
         className="box"
-        initial={{ opacity: 0.5, scale: 0.5 }}
-        transition={{ duration: 2 }}
-        whileInView={{ opacity: 1, scale: 2 }}
-        drag
+        variants={variants}
+        // initial="hidden"
+        // animate="visible"
+        animate={ open ? "visible" :  "hidden" }
       ></motion.div>
+      <button onClick={() => setOpen(prev => !prev)}>Click</button>
     </div>
   );
 };
