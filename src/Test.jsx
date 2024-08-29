a3445ecea19b7f2201407fe699cb4475aabd2893
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-08-29 16:31:30 +0700

    Motion List Test

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,23 +1,25 @@
 import { motion } from "framer-motion";
-import { useState } from "react";
 
 const Test = () => {
-    const [open, setOpen] = useState(false);
 
-    const variants = {
-        visible: {opacity: 1, x: 500, transition: {type: "spring", stiffness: 100, damping: 50 }},
-        hidden: {opacity: 0}
-    }
+  const variants = {
+    visible: {
+      opacity: 1,
+      x: 100,
+      transition: { staggerChildren: 0.5 },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  const items = ["item1", "item2", "item3", "item4"];
+
   return (
     <div className="course">
-      <motion.div
-        className="box"
-        variants={variants}
-        // initial="hidden"
-        // animate="visible"
-        animate={ open ? "visible" :  "hidden" }
-      ></motion.div>
-      <button onClick={() => setOpen(prev => !prev)}>Click</button>
+      <motion.ul variants={variants} initial='hidden' animate='visible'>
+        {items.map((item) => (
+          <motion.li variants={variants} key={item}>{item}</motion.li>
+        ))}
+      </motion.ul>
     </div>
   );
 };
