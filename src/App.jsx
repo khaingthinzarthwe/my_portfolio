2a4e358d9d930b0903f7b9dd08a6f4ac0b0daba9
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-09-09 13:36:16 +0700

    Contact Section Text Container Design

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,10 @@ const App = () => {
         <Navbar />
         <Orange />
       </section>
-      {/* <section id="Services"><Universe type='services' /></section>
-      <section>Services</section> */}
       <section id="Projects"><Universe  /></section>
       <Portfolio />
       <section id="Contact"><Contact /></section>
-      <section id="About">Contact</section>
+      <section id="About">About</section>
     </div>
   );
 };
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,27 @@ import './contact.scss';
 
 const Contact = () => {
   return (
-    <div className='contact'>Contact</div>
+    <div className='contact'>
+        <div className="textContainer">
+            <h1>Let&apos;s Work Together</h1>
+            <div className="item">
+                <h2>Mail</h2>
+                <span>khaingthinzarthwe.dev@gmail.com</span>
+            </div>
+            <div className="item">
+                <h2>Address</h2>
+                <span>Nonthaburi, Thailand</span>
+            </div>
+        </div>
+        <div className="formContainer">
+            <form>
+                <input type='text' required placeholder='Name' />
+                <input type='email' required placeholder='Emaill' />
+                <textarea rows={8} placeholder='Message' />
+                <button>Submit</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
