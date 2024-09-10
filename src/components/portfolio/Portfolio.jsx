795f1562ce9f3bcaabb35d5ded46328902242acd
yoh yoh <khaingthinzarthwe.dev@gmail.com>
2024-09-10 15:52:25 +0700

    Change Projects Image

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,20 +6,20 @@ const projects = [
   {
     id: 1,
     title: "Game Hub Version 1",
-    img: "https://images.pexels.com/photos/25568845/pexels-photo-25568845/free-photo-of-couple-standing-and-reading-album.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, repellendus, inventore dolores asperiores distinctio nesciunt obcaecati deserunt dolorum voluptatum quasi molestiae nulla esse saepe voluptatibus fuga exercitationem. Exercitationem, dolor ad.",
+    img: "./gamehubv1.png",
+    desc: "",
   },
   {
     id: 2,
     title: "Game Hub Version 2",
-    img: "https://images.pexels.com/photos/3855560/pexels-photo-3855560.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, repellendus, inventore dolores asperiores distinctio nesciunt obcaecati deserunt dolorum voluptatum quasi molestiae nulla esse saepe voluptatibus fuga exercitationem. Exercitationem, dolor ad.",
+    img: "./gamehubv2.png",
+    desc: "",
   },
   {
     id: 3,
     title: "Evo Gym",
-    img: "https://images.pexels.com/photos/28197211/pexels-photo-28197211/free-photo-of-a-person-riding-a-horse-on-a-dirt-road.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, repellendus, inventore dolores asperiores distinctio nesciunt obcaecati deserunt dolorum voluptatum quasi molestiae nulla esse saepe voluptatibus fuga exercitationem. Exercitationem, dolor ad.",
+    img: "./evogym.png",
+    desc: "",
   },
 ];
 
